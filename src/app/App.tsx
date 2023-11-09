112b528e4305dfcc7a746c22950ea29db9f0e4a1
Andrey <butor.2011@mail.ru>
2023-11-09 23:25:12 +0700

    add sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
 const App = () => {
     const { theme } = useTheme();
@@ -12,7 +13,11 @@ const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar />
-            <AppRouter />
+            <div className='content-page'>
+                <Sidebar />
+                <AppRouter />
+            </div>
+
         </div>
     );
 };
