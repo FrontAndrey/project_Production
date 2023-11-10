8063d9ecfe7f292d4c50fe464166ea6cc4cfb115
Andrey <butor.2011@mail.ru>
2023-11-11 00:51:58 +0700

    add internationalization

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,26 @@
 import React, { useContext, useState, Suspense } from 'react';
 import './styles/index.scss';
-import { Link, Route, Routes } from 'react-router-dom';
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { useTranslation } from 'react-i18next';
+
 
 const App = () => {
     const { theme } = useTheme();
 
     return (
-        <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className='content-page'>
-                <Sidebar />
-                <AppRouter />
-            </div>
 
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback=''>
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
