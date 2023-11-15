d915c3d362c5c10378404c1be6cd3e12cf233273
Andrey <butor.2011@mail.ru>
2023-11-15 22:23:21 +0700

    add jest, add first test classNames

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
-import React, { useContext, useState, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import './styles/index.scss';
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { useTranslation } from 'react-i18next';
 
 
 const App = () => {
